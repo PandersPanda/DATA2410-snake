770c2bf5c17dabce9b3dd7f94c4348d857db8f77
Uy Quoc Nguyen <s341864@oslomet.no>
2021-04-26 16:08:18 +0200

    Snake grpc implementation

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 import tkinter
+import grpc
 import snake_pb2
 import snake_pb2_grpc
-import grpc
-import random
 
 SNAKE_SIZE = 20
 GAME_SPEED = 50
@@ -16,107 +15,57 @@ root.title("Snake Game")
 
 canvas = tkinter.Canvas(width=GAME_WIDTH, height=GAME_HEIGHT, highlightthickness=0, background='black')
 
-snake = snake_pb2.Snake(
-    name="test_player",
-    color="Red",
-    size=3,
-    direction="Right",
-    body=[
-        snake_pb2.Point(x=2, y=0),  # Head
-        snake_pb2.Point(x=1, y=0),  # Body
-        snake_pb2.Point(x=0, y=0)  # Tail
-    ]
-)
-
-food = snake_pb2.Food(color='White', position=snake_pb2.Point(x=10, y=10))
-
 channel = grpc.insecure_channel('localhost:50051')
-stub = snake_pb2_grpc.SnakeGameServiceStub(channel)
-stub.addSnake(snake)
+stub = snake_pb2_grpc.SnakeServiceStub(channel)
+
+snake = stub.addSnake(snake_pb2.JoinRequest())
+direction = snake.direction
 
 
-def draw_snake():
-    global canvas
-    global snake
-
-    canvas.delete("snake")
-
-    for node in snake.body:
+def draw_snake(s):
+    for p in s.body:
         canvas.create_rectangle(
-            node.x * SNAKE_SIZE,
-            node.y * SNAKE_SIZE,
-            (node.x + 1) * SNAKE_SIZE,
-            (node.y + 1) * SNAKE_SIZE,
-            fill=snake.color,
-            tag="snake"
+            p.x * SNAKE_SIZE,
+            p.y * SNAKE_SIZE,
+            (p.x + 1) * SNAKE_SIZE,
+            (p.y + 1) * SNAKE_SIZE,
+            fill=s.color,
+            tag='snake'
         )
 
 
-draw_snake()
-
-
-def set_new_snake_direction(event):
-    global snake
-    snake.direction = event.keysym
-    snake = stub.moveSnake(snake)
-
-
-canvas.bind_all('<Key>', set_new_snake_direction)
-
-
-def collision_check():
-    global food, snake, root
-
-    head_x = snake.body[0].x
-    head_y = snake.body[0].y
-
-    if snake.body[0] == food.position:
-        new_head = snake_pb2.Point(x=head_x, y=head_y)
-        snake.body.insert(0, new_head)
-        stub.eatFood(snake)
-        spawn_food()
-        return False
-    return head_x < 0 or head_x > 30 or head_y < 0 or head_y > 30
+draw_snake(snake)
 
 
 def move_snake():
     global snake
-    if collision_check():
-        return
-
-    snake = stub.moveSnake(snake)
-    draw_snake()
-
-    # Collision check:
-    canvas.after(GAME_SPEED, move_snake)
-
-
-def spawn_food():
-    global canvas
-    global food
-    canvas.delete("food")
-
-    food_x = random.randint(0, GAME_WIDTH // SNAKE_SIZE - 1)
-    food_y = random.randint(0, GAME_HEIGHT // SNAKE_SIZE - 1)
-
-    while snake_pb2.Point(x=food_x, y=food_y) in snake.body:
-        food_x = random.randint(0, GAME_WIDTH // SNAKE_SIZE - 1)
-        food_y = random.randint(0, GAME_HEIGHT // SNAKE_SIZE - 1)
-        snake_pb2.Point(x=food_x, y=food_y)
-
-    food = stub.spawnFood(snake_pb2.Point(x=food_x, y=food_y))
-
-    canvas.create_rectangle(
-        food_x * SNAKE_SIZE,
-        food_y * SNAKE_SIZE,
-        (food_x + 1) * SNAKE_SIZE,
-        (food_y + 1) * SNAKE_SIZE,
-        fill=food.color,
-        tag="food"
+    snake = stub.moveSnake(
+        snake_pb2.MoveRequest(color=snake.color, direction=direction)
     )
 
 
-spawn_food()
+def change_direction(event):
+    global direction
+    direction = event.keysym
+
+
+def draw_All_Snakes():
+    canvas.delete('snake')
+    snakes = stub.getSnakes(snake_pb2.GetRequest())
+    for s in snakes:
+        draw_snake(s)
+
+
+canvas.bind_all('<Key>', change_direction)
+
+
+def game_flow():
+    global snake
+    move_snake()
+    draw_All_Snakes()
+    canvas.after(GAME_SPEED, game_flow)
+
+
+game_flow()
 canvas.pack()
-canvas.after(GAME_SPEED, move_snake)
 root.mainloop()
--- server.py
+++ server.py
@@ -1,59 +1,55 @@
-import snake_pb2
-import snake_pb2_grpc
 import grpc
+import snake_pb2_grpc
+from snake_pb2 import Snake, Point
 from concurrent import futures
 
 
-class SnakeGame(snake_pb2_grpc.SnakeGameServiceServicer):
+class SnakeGame(snake_pb2_grpc.SnakeServiceServicer):
     SNAKES = {}
+    AVAILABLE_COLORS = ['Blue', 'Red']
 
     def addSnake(self, request, context):
-        self.SNAKES.update({request.name: request})
-        return request
-
-    def locateOtherSnakes(self, request, context):
-        for snake in self.SNAKES:
-            if request != snake:
-                yield snake
-
-    def spawnFood(self, request, context):
-        return snake_pb2.Food(color='white', position=request)
-
-    def moveSnake(self, request, context):
-        snake = self.SNAKES.get(request.name, None)
-        opposite_direction = [{'Up', 'Down'}, {'Right', 'Left'}]
-
-        if {snake.direction, request.direction} in opposite_direction:
-            return snake
-
-        head_x_position = snake.body[0].x
-        head_y_position = snake.body[0].y
-
-        if request.direction == "Right":
-            new_position = snake_pb2.Point(x=head_x_position + 1, y=head_y_position)
-            snake.body.insert(0, new_position)
-        if request.direction == "Left":
-            new_position = snake_pb2.Point(x=head_x_position - 1, y=head_y_position)
-            snake.body.insert(0, new_position)
-        if request.direction == "Down":
-            new_position = snake_pb2.Point(x=head_x_position, y=head_y_position + 1)
-            snake.body.insert(0, new_position)
-        if request.direction == "Up":
-            new_position = snake_pb2.Point(x=head_x_position, y=head_y_position - 1)
-            snake.body.insert(0, new_position)
-
-        snake.body.pop()
-        snake.direction = request.direction
+        snake = Snake(
+            color='Red',
+            direction='Right',
+            body=[Point(x=2, y=0), Point(x=1, y=0), Point(x=0, y=0)]
+        )
+        self.SNAKES.update({snake.color: snake})
         return snake
 
-    def eatFood(self, request, context):
-        self.SNAKES.update({request.name: request})
-        return request
+    def moveSnake(self, request, context):
+        # MoveRequest(color= ... , direction=...)
+        new_direction = request.direction
+        snake = self.SNAKES.get(request.color, None)
+        opposite_direction = [{'Up', 'Down'}, {'Left', 'Right'}]
+        if {snake.direction, new_direction} in opposite_direction:
+            new_direction = snake.direction
+        # [Point(x=2, y=0), Point(x=1, y=0), Point(x=0, y=0)]
+        new_head = Point(x=snake.body[0].x, y=snake.body[0].y)
+
+        if new_direction == "Right":
+            new_head.x += 1
+        elif new_direction == "Left":
+            new_head.x -= 1
+        elif new_direction == "Down":
+            new_head.y += 1
+        elif new_direction == "Up":
+            new_head.y -= 1
+
+        snake.body.pop()
+        snake.body.insert(0, new_head)
+        snake.direction = new_direction
+
+        return snake
+
+    def getSnakes(self, request, context):
+        for snake in self.SNAKES.values():
+            yield snake
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    snake_pb2_grpc.add_SnakeGameServiceServicer_to_server(
+    snake_pb2_grpc.add_SnakeServiceServicer_to_server(
         SnakeGame(), server
     )
     server.add_insecure_port('[::]:50051')
